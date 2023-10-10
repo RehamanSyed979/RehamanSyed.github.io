1c3a213da171272ace1eb0595e0e2002d8677dff
RehamanSyed979 <rehamansyed979@gmail.com>
2023-10-10 14:37:29 +0530

    Website

---
+++ animated_portfolio_website-main/src/components/resume/Resume.jsx
@@ -0,0 +1,205 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div id="resume" className=" container m-auto   mt-16">
+      {/* heading */}
+      <div data-aos="fade-up" className="relative mb-5">
+        <h3 className=" text-3xl font-black text-gray-400 sm:text-2xl">
+          Resume
+        </h3>
+        <span className="h-[1.1px] right-0 absolute w-[92%] bg-gray-300 block"></span>
+      </div>
+      <div data-aos="fade-up" className="left flex-1 w-full">
+        <p className=" text-gray-700 font-medium w-[100%]">
+          Here are my experiences and qualifications.
+        </p>
+      </div>
+      {/* card*/}
+      <div className="card-wrapper w-[90%] sm:w-full mt-5 flex md:flex-col sm:gap-5 mx-auto ">
+        <div className="left flex-1 flex items-center justify-center">
+          <fieldset
+            data-aos="zoom-in"
+            className=" w-[80%] p-5 py-12 sm:py-8 sm:w-full sm:p-2"
+          >
+            <legend className=" w-auto ml-[50%] translate-x-[-50%] border-2 border-gray-200 rounded-3xl py-1 px-8 font-semibold text-xl text-yellow-500">
+              Experiences
+            </legend>
+            <div className=" relative">
+              {/* design */}
+              <div className="design flex absolute left-[-150px] top-1/2 items-center rotate-[90deg] sm:left-[-160px] ">
+                <div className="c1 w-[12px] h-[12px] rounded-full bg-white border-2 border-yellow-500"></div>
+                <div className="line w-[230px] bg-gray-300 h-[2px] sm:w-[250px]"></div>
+                <div className="c2 w-[12px] h-[12px] rounded-full bg-white border-2 border-yellow-500"></div>
+              </div>
+              {/* design */}
+              <div className=" flex flex-col gap-1 sm:gap-1 border-2 border-yellow-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
+                <h1 className="text-[1.4rem] font-semibold sm:text-xl">
+                Senior Software Engineer UI UX Designer
+                </h1>
+                <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
+                  Company Name: Capgemini Technology Services India Limited
+                </span>
+                <span className=" text-[.9rem] font-semibold text-yellow-500 sm:text-base">
+                  NOV 2021 - Present
+                </span>
+                <p className=" text-[.9rem] text-justify break-words text-gray-500">
+                Collaborated closely with senior designers and cross
+functional teams to understand requirements and translate 
+them into visually stunning user interface designs. 
+
+
+• Created wireframes, prototypes, and visual mock ups 
+using Adobe XD, Illustrator, and Figma to effectively 
+communicate design concepts. 
+
+• Leveraged illustration skills to craft captivating graphics 
+and visual assets, enriching user engagement and 
+reinforcing brand identity. 
+
+• Continuously iterated and enhanced designs to meet 
+rigorous standards, resulting in heightened user satisfaction 
+and garnering positive feedback.
+                </p>
+              </div>
+              <div>
+              <span>&nbsp;</span>
+              </div>
+              <legend className=" w-auto ml-[50%] translate-x-[-50%] border-2 border-gray-200 rounded-3xl py-1 px-8 font-semibold text-xl text-yellow-500">
+              Qualifications
+            </legend>
+            <div>
+              <span>&nbsp;</span>
+              </div>
+            </div>
+            <div className=" relative">
+              {/* design */}
+              <div className="design flex absolute left-[-150px] top-1/2 items-center rotate-[90deg] sm:left-[-160px] ">
+                <div className="c1 w-[12px] h-[12px] rounded-full bg-white border-2 border-yellow-500"></div>
+                <div className="line w-[230px] bg-gray-300 h-[2px] sm:w-[250px]"></div>
+                <div className="c2 w-[12px] h-[12px] rounded-full bg-white border-2 border-yellow-500"></div>
+              </div>
+              {/* design */}
+              <div className=" flex flex-col gap-1 sm:gap-1 border-2 border-yellow-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
+                <h1 className="text-[1.4rem] font-semibold sm:text-xl">
+                B.Tech,  Electrical and Electronics Engineering
+                </h1>
+                <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
+                  College Name:Usha Rama College of Engineering And 
+Technology 
+                </span>
+                <span className=" text-[.9rem] font-semibold text-yellow-500 sm:text-base">
+                  Year 2017 - Year 2020
+                </span>
+                <p className=" text-[.9rem] text-justify break-words text-gray-500">
+                I graduated in September 2020 with a GPA of 6.69 in 
+Electrical and Electronics Engineering. 
+                </p>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+        <div className="right flex-1 flex items-center justify-center">
+          <fieldset
+            data-aos="zoom-in"
+            className=" w-[80%] p-5 py-12 sm:py-8 sm:w-full sm:p-2"
+          >
+           
+            <div className=" relative">
+              {/* design */}
+              <div className="design flex absolute left-[-150px] top-1/2 items-center rotate-[90deg] sm:left-[-160px] ">
+                <div className="c1 w-[12px] h-[12px] rounded-full bg-white border-2 border-yellow-500"></div>
+                <div className="line w-[230px] bg-gray-300 h-[2px] sm:w-[250px]"></div>
+                <div className="c2 w-[12px] h-[12px] rounded-full bg-white border-2 border-yellow-500"></div>
+              </div>
+              <div>
+    {/* Add empty space with CSS styles */}
+    <div style={{ width: '100px', height: '80px' }}></div>
+
+   
+  </div>
+              {/* design */}
+              <div className=" flex flex-col gap-1 border-2 border-yellow-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
+                <h1 className="text-[1.4rem] font-semibold sm:text-xl">
+                Associate Analyst UX UI Designer
+                </h1>
+                <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
+                Company Name: Adwaitham Global IT Solutions Pvt. Ltd
+                </span>
+                <span className=" text-[.9rem] font-semibold text-yellow-500 sm:text-base">
+                Sept 2020 - NOV 2021
+                </span>
+                <p className=" text-[.9rem] text-justify text-gray-500">
+                Collaborated with cross-functional teams to create visually 
+appealing and captivating designs for concept and 
+production vehicles and related digital platforms.
+
+
+ • Designed user centered interfaces using Adobe Illustrator, 
+Adobe XD, and Figma resulting in improved user experience 
+and increased customer satisfaction. 
+
+• Conducted trend reports on the latest UI/UX developments, 
+emerging technologies, and techniques, providing valuable 
+insights to the design team. 
+
+• Assisted in creating visual and interaction design, 
+maintaining consistency with a cohesive style guide.
+                </p>
+              </div>
+              
+          
+  <div>
+    {/* Add empty space with CSS styles */}
+    <div style={{ width: '90px', height: '90px' }}></div>
+
+   
+  </div>
+
+
+
+ 
+            
+            </div>
+            <div className=" relative">
+              {/* design */}
+              <div className="design flex absolute left-[-150px] top-1/2 items-center rotate-[90deg] sm:left-[-160px] ">
+                <div className="c1 w-[12px] h-[12px] rounded-full bg-white border-2 border-yellow-500"></div>
+                <div className="line w-[230px] bg-gray-300 h-[2px] sm:w-[250px]"></div>
+                <div className="c2 w-[12px] h-[12px] rounded-full bg-white border-2 border-yellow-500"></div>
+              </div>
+              {/* design */}
+              <div className=" flex flex-col gap-1 border-2 border-yellow-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
+                <h1 className="text-[1.4rem] font-semibold sm:text-xl">
+                Diploma,  Electrical and Electronics Engineering
+                </h1>
+                <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
+                College Name:Usha Rama College of Engineering And 
+Technology 
+                </span>
+                <span className=" text-[.9rem] font-semibold text-yellow-500 sm:text-base">
+                  Year 2014 - Year 2020
+                </span>
+                <p className=" text-[.9rem] text-justify text-gray-500">
+                I Completed my Diploma in September 2020 with a 
+Percentage of 73% in Electrical and Electronics 
+Engineering.
+                </p>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+
+    
+      </div>
+    </div>
+   
+  );
+};
+
+export default Resume;
+
+
+
+
+
